1d0b545278b8bd959c92095af0e99337fefdfaec
varshithakarthik <82289117+varshithakarthik@users.noreply.github.com>
2021-11-27 20:41:06 -0500

    Update Ground.js

--- Ground.js
+++ Ground.js
@@ -2,21 +2,21 @@ class Ground {
   constructor(x, y, width, height) {
     var options = {
       isStatic: true,
-      'restitution':0,
-      'friction':0,
-      'density':1
+      'restitution':0.3,
+      'friction':0.5,
+      'density':1.2
     }
-    this.body = Bodies.rectangle(x, y, width, height, options);
-    this.width = width;
-    this.height = height;
+    this.body = Bodies.rectangle(x, y,1200,20, options); 
       
-    World.add(world, this.body);
+    World.add(world, this.ground);
     }
   display(){
+    push();
     rectMode(CENTER);
 
     fill(255);
 
     rect(this.body.position.x, this.body.position.y, this.width, this. height);
+                pop();
     }
   };
