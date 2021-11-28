3a40bb84e336dae6a5a323855ea13d329e1fb0cc
varshithakarthik <82289117+varshithakarthik@users.noreply.github.com>
2021-11-27 20:37:44 -0500

    Update Dustbin.js

--- Dustbin.js
+++ Dustbin.js
@@ -6,17 +6,23 @@ class Dustbin {
           'friction':1,
           'density':0.1
       }
-      this.body = Bodies.rectangle(x, y, width, height, options);
-      this.width = width;
-      this.height = height;
-      
-      World.add(world, this.body);
+        this.leftbody=Bodies.rectangle(x-100+10+40, y-60-10-50,20,100, options)
+        this.rightbody=Bodies.rectangle(x+100-40, y-60-10-50,20,100, options)
+        this.centerbody=Bodies.rectangle(x, y-60,100,20, options)
+        this.image=loadImage("dustbingreen.png");
+        World.add(world, this.leftbody);
+        World.add(world, this.rightbody);
+        World.add(world, this.centerbody);
     }
     display(){
       rectMode(CENTER);
-
+      push();
       fill(255);
-
-      rect(this.body.position.x, this.body.position.y, this.width, this. height);
+      imageMode(CENTER);
+        image(this.image,this.centerbody.position.x,this.centerbody.position.y-50,100, 100)
+        rect(this.centerbody.position.x,this.centerbody.position.y,100,20)
+        rect(this.leftbody.position.x,this.leftbody.position.y,20,100)
+        rect(this.rightbody.position.x,this.rightbody.position.y,20,100)
+        pop();
     }
   };
